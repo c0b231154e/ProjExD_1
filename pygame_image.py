630cec4ed55bdc4f36155c17ccd98134a266b232
sososo <c0b231154e@edu.teu.ac.jp>
2024-11-19 16:36:41 +0900

    8まで

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,39 @@ def main():
     koka_img = pg.image.load("fig/3.png")
     koka_img = pg.transform.flip(koka_img,True,False)
     tmr = 0
+    kk_rct = koka_img.get_rect()
+    kk_rct.center = 300, 200
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        key_lst = pg.key.get_pressed()
+
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+
 
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_flip_img,[x + 1600,0])
-        screen.blit(bg_img, [3200, 0])
-        screen.blit(bg_flip_img,[4800,0])
-        screen.blit(koka_img,[300,200])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_flip_img,[x+4800,0])
+
+        #screen.blit(koka_img,[300,200])
+
+        screen.blit(koka_img, kk_rct)
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)
